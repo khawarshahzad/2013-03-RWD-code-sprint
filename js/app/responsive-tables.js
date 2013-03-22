649855b4fc96f57aa23746b424eb882b9bfe42d6
Craig Patik <cpatik@gmail.com>
2013-03-22 16:11:59 -0400

    Resp tables bug fixes and cleanup
    Restructured the code, removed the options

--- js/app/responsive-tables.js
+++ js/app/responsive-tables.js
@@ -35,15 +35,11 @@ var toggleTable = (function _toggleTable() {
   $.widget( "filament.toggleTable", { // need to come up with a better namespace var...
 
     options: {
-      idprefix: null,   // specify a prefix for the id/headers values
-      persist: null, // specify a class assigned to column headers (th) that should always be present; the script not create a checkbox for these columns
-      checkContainer: null // container element where the hide/show checkboxes will be inserted; if none specified, the script creates a menu
     },
 
     // Set up the widget
     _create: function() {
       var self = this,
-          o = self.options,
           $table = self.element,
           $thead = $table.find('thead'),
           $tbody = $table.find('tbody'),
@@ -53,20 +49,6 @@ var toggleTable = (function _toggleTable() {
           $container = $('#' + $table.attr('id') + '-check-container'),
           dropdownid = 'drop-' + $table.attr('id'),
           totalMinWidth = 0;
-
-      // Add missing optional classes and attributes to table
-      $hdrCols.each(function(i) {
-        var $th = $(this);
-
-        if (!$th.attr('data-min-width')) {
-          // Assign current width
-          $th.data('min-width', $th.width());
-        }
-
-        if (!$th.is('.persist, .priority1, .priority2')) {
-          $th.addClass('priority2');
-        }
-      });
 
       // Test if element is currently displaying
       function isDisplaying ($elem) {
@@ -105,16 +87,14 @@ var toggleTable = (function _toggleTable() {
           // Hide the right-most `selector` column
           $hdrColsReversed.each(function() {
             var $this = $(this),
-                $input;
+                $input = $('input[value="' + $this.attr('id') + '"]');
 
-            if (isDisplaying($this) && $this.is(selector)) {
+            if (!$input.data('user-changed') && isDisplaying($this) && $this.is(selector)) {
               // Hide cell
               $('#' + $this.attr('id') + ', [headers="' + $this.attr('id') + '"]').hide();
 
-              // Uncheck and hide the toggle
-              $input = $('input[value="' + $this.attr('id') + '"]');
-              $input.parent().hide();
-              $input.attr('checked', false);
+              // Uncheck the toggle
+              $input.prop('checked', false);
 
               // Update and re-check widths
               totalMinWidth = getTotalMinWidth();
@@ -141,18 +121,16 @@ var toggleTable = (function _toggleTable() {
           // Hide the right-most `selector` column
           $hdrColsReversed.each(function() {
             var $this = $(this),
-                $input;
+                $input = $('input[value="' + $this.attr('id') + '"]');
 
-            if (!isDisplaying($this) && $this.is(selector)) {
+            if (!$input.data('user-changed') && !isDisplaying($this) && $this.is(selector)) {
               // Determine if this min width would exceed the allowed space
               if (totalMinWidth + $this.data('min-width') <= allowedWidth) {
                 // Show cell
                 $('#' + $this.attr('id') + ', [headers="' + $this.attr('id') + '"]').show();
 
-                // Check and show the toggle
-                $input = $('input[value="' + $this.attr('id') + '"]');
-                $input.attr('checked', true);
-                $input.parent().show();
+                // Check the toggle
+                $input.prop('checked', true);
               }
 
               // Check new width
@@ -165,13 +143,25 @@ var toggleTable = (function _toggleTable() {
           });
         });
 
-        // Check if columns need to be hid
+        // Check if columns need to be hidden
         hideCols();
       } // end showCols()
 
-      $container.append('<ul id="' + dropdownid + '" class="f-dropdown toggle-table-dropdown"></ul>');
+      // Add missing optional classes and attributes to table
+      $hdrCols.each(function(i) {
+        var $th = $(this);
 
-      o.persist = o.persist || 'persist';
+        if (!$th.attr('data-min-width')) {
+          // Assign current width
+          $th.data('min-width', $th.width());
+        }
+
+        if (!$th.is('.persist, .priority1, .priority2')) {
+          $th.addClass('priority2');
+        }
+      });
+
+      $container.append('<ul id="' + dropdownid + '" class="f-dropdown toggle-table-dropdown"></ul>');
 
       $hdrCols.each(function(i) {
         var th = $(this),
@@ -180,7 +170,7 @@ var toggleTable = (function _toggleTable() {
 
         // assign an id to each header, if none is in the markup
         if (!id) {
-          id = ( o.idprefix ? o.idprefix : 'col-' ) + i;
+          id = 'col-' + i;
           th.attr('id', id);
         }
 
@@ -193,34 +183,29 @@ var toggleTable = (function _toggleTable() {
         });
 
         // create the hide/show toggles
-        if ( !th.is('.' + o.persist) ) {
+        if ( !th.is('.persist') ) {
           var $toggle = $('<li><input type="checkbox" name="toggle-cols" id="toggle-col-'+i+'" value="'+id+'"><label for="toggle-col-'+i+'">'+th.text()+'</label></li>');
 
           $container.find('ul').append($toggle);
 
           $toggle
             .find('input')
-            .change(function(){
-              var $input = $(this),
-                  val = $input.val(),
-                  cols = $('#' + val + ', [headers="' + val + '"]');
+              .prop('checked', true) // Check all by default
+              .on('change', function(){
+                var $input = $(this),
+                    val = $input.val(),
+                    cols = $('#' + val + ', [headers="' + val + '"]');
 
-              if ($input.is(':checked')) {
-                cols.show();
-              }
-              else {
-                cols.hide();
-              }
-            })
-            .on('updateCheck', function(){
-              if ( isDisplaying(th) ) {
-                $(this).attr('checked', true);
-              }
-              else {
-               $(this).attr('checked', false);
-              }
-            })
-            .trigger('updateCheck');
+                // Track user's manual changes so they're not overridden
+                $input.data('user-changed', true);
+
+                if ($input.is(':checked')) {
+                  cols.show();
+                }
+                else {
+                  cols.hide();
+                }
+              });
         }
       }); // end $hdrCols loop
 
@@ -228,11 +213,11 @@ var toggleTable = (function _toggleTable() {
 
       // update the inputs' checked status
       $(window).on('orientationchange resize', function() {
-        //$container.find('input').trigger('updateCheck');
         showCols();
       });
 
-      hideCols();
+      // Update the view now
+      showCols();
 
       $container.prepend('<a href="#" data-dropdown="' + dropdownid + '" class="small button dropdown">Display</a>');
     } // end _create

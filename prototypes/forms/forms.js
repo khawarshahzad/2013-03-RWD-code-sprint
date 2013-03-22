7291b30c6998ce9c9e82dd35063d2680d8db9b18
Craig Patik <cpatik@gmail.com>
2013-03-21 20:51:41 -0400

    Better alignment

--- prototypes/forms/forms.js
+++ prototypes/forms/forms.js
@@ -1,36 +1,23 @@
-var forms = (function _forms () {
-  function init () {
-    $('input, select, textarea').on('change blur', function(evt) {
-      var $this = $(this),
-          $container = $this.closest('.columns');
-      if (this.validity) {
-        if (this.validity.valid === true && this.willValidate === true) {
-          console.log('is valid AND will validate: ', this);
-          $container.removeClass('error');
-        }
-        else if (this.validity.valid === true) {
-          console.log('is valid: ', this);
-          $container.removeClass('error');
-        }
-        else {
-          console.log('NOT valid: ', this);
-          $container.addClass('error');
-        }
-      }
-      else if (this.willValidate === true) {
-        console.log('valid: ', this);
+$(document).ready(function () {
+
+  // Check form field validity and mark errors
+  // Based on browser validation
+  $('input, select, textarea').on('blur', function(evt) {
+    var $container = $(this).closest('.columns');
+
+    if (this.validity) {
+      if (this.validity.valid === true) {
         $container.removeClass('error');
       }
-      else if (this.willValidate === false) {
-        console.log('NOT valid: ', this);
+      else {
         $container.addClass('error');
       }
-    });
-  }
-
-  return {
-    init: init
-  };
-}());
-
-$(document).ready(forms.init);
+    }
+    else if (this.willValidate === true) {
+      $container.removeClass('error');
+    }
+    else if (this.willValidate === false) {
+      $container.addClass('error');
+    }
+  });
+});

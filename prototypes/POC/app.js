0816e8f76ec51c52f211851d331bedb82d54c4cd
Jeff Herb <herb.jeff@gmail.com>
2013-03-22 13:14:58 -0400

    Stated adding site header interaction

--- prototypes/POC/app.js
+++ prototypes/POC/app.js
@@ -1,6 +1,8 @@
 // Active Governor Banner
 $('#gov-pull-down').on('click', function() {
 
+	e.preventDefault();
+
 	// Check to see if the governor banner is show or not
 	if ($('html').hasClass('active-gov-bar')) {
 
@@ -14,8 +16,27 @@ $('#gov-pull-down').on('click', function() {
 });
 
 // Standard Gov Banner display code
-$('#gov-link-3').on('click', function() {
+$('#gov-link-3').on('click', function(e) {
+
+	e.preventDefault();
 
 	$('html').addClass('active-gov-bar-search');
+
+});
+
+// Activate the search bar on mobile
+$('#mobile-site-search').on('click', function(e) {
+
+	e.preventDefault();
+
+	if ($('html').hasClass('active-mobile-site-search')) {
+
+		$('html').removeClass('active-mobile-site-search')
+
+	} else {
+
+		$('html').addClass('active-mobile-site-search')
+		
+	}
 
 });

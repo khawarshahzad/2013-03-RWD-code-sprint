33257524dd18871b465eda43c307248af95afb4f
Craig Patik <cpatik@gmail.com>
2013-03-26 16:24:29 -0400

    Location updates

--- prototypes/location/app.js
+++ prototypes/location/app.js
@@ -9,13 +9,15 @@ $(document).ready(function(){
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
       $img.attr('src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + latitude + ',' + longitude + '&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C' + latitude + ',' + longitude + '&sensor=false');
-    }, showDefaultMap);
+    }, showDefaultMap('You\'re not allowing your location to be found.'));
   }
   else {
     showDefaultMap();
   }
 
-  function showDefaultMap() {
+  function showDefaultMap(msg) {
+    msg = msg || 'Your device does not support geolocation.';
+    $('#status').html(msg);
     $img.attr('src', 'http://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Ccolor:red%7Clabel:C%7C40.718217,-73.998284&sensor=false');
   }
 

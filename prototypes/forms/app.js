e036e60958981be89f10ec90dc1de0cf6b84b26d
Craig Patik <cpatik@gmail.com>
2013-03-26 11:04:09 -0400

    Generic italic and bold classes
    For using the custom fonts

--- prototypes/forms/app.js
+++ prototypes/forms/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   // Based on browser validation
   $('input, select, textarea').on('blur', function(evt) {
     var $container = $(this).closest('.columns'),
-        classNames = 'error show-form-field-info';
+        classNames = 'error show';
 
     if (this.validity) {
       if (this.validity.valid === true) {

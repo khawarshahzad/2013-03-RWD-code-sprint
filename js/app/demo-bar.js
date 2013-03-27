033399cac1b664a682f15b328b0f7257213375ca
Craig Patik <cpatik@gmail.com>
2013-03-27 11:06:10 -0400

    Demo bar fixes

--- js/app/demo-bar.js
+++ js/app/demo-bar.js
@@ -1,10 +1,12 @@
+/*global Modernizr: false */
+
 $(function(){
-  if ( parent.$("#devices").length <=0 && window.matchMedia("(min-width: 48em)").matches ) {
+  if ( parent.$("#devices").length <=0 && !Modernizr.touch ) {
       (function(){
-        var d=document;
+        var d = document;
         d.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>'+
         d.title+
-        '<\/title><link rel="stylesheet" href="../../css/foundation.css"><link rel="stylesheet" href="../../css/app.css"><\/head>' +
+        '<\/title><link rel="stylesheet" href="../css/foundation.css"><link rel="stylesheet" href="../css/app.css"><\/head>' +
         '<body id="resize-bar-body">' +
         '<header>' +
         //'<div class="close"><a href="#">X<\/a><\/div>' +
@@ -13,7 +15,7 @@ $(function(){
         //'<div class="cssrefresh"><a href="#">I<\/a><\/div>' +
         '<div id="devices">' +
         '  <a href="#" class="tablet-landscape"><span>Tablet Landscape<\/span><\/a>' +
-        '  <a href="#" class="tablet-portrait"><span>Tablet Portrait<\/span><\/a>' +
+        '  <a href="#" class="tablet-portrait" id="tablet-landscape-joyride"><span>Tablet Portrait<\/span><\/a>' +
         '  <a href="#" class="smartphone-landscape"><span>iPhone Landscape<\/span><\/a>' +
         '  <a href="#" class="smartphone-portrait"><span>iPhone Portrait<\/span><\/a>' +
         '  <a href="#" id="desktop-view" class="auto active"><span>Desktop<\/span><\/a>' +
@@ -22,18 +24,18 @@ $(function(){
         '   <h4>Getting Started!<\/h4>'+
         '   <p>This tutorial will teach you how to use the "Resize Bar" available throughout the demo site.<\/p>'+
         '  <\/li>'+
-        '  <li data-id="devices">'+
+        '  <li data-id="tablet-landscape-joyride">'+
         '   <h4>Choose a Device<\/h4>'+
         '   <p>Click any of these buttons to see how this site would look on various devices.<\/p><p><strong>Go ahead, click one now!<\/strong><\/p>'+
         '  <\/li>'+
-        '  <li data-button="Next">'+
+        '  <li data-button="Yes!">'+
         '   <h4>Notice a Difference?<\/h4>'+
         '   <p>You are now viewing the same page at a smaller size.<\/p>'+
         '  <\/li>'+
-        '  <li data-id="keyboard" data-button="Next">'+
-        '   <h4>Show a Keyboard<\/h4>'+
-        '   <p>Clicking this button will show/hide a keyboard overlay.<\/p>'+
-        '  <\/li>'+
+        // '  <li data-id="keyboard" data-button="Next">'+
+        // '   <h4>Show a Keyboard<\/h4>'+
+        // '   <p>Clicking this button will show/hide a keyboard overlay.<\/p>'+
+        // '  <\/li>'+
         '  <li data-id="desktop-view" data-button="Close">'+
         '   <h4>Desktop View<\/h4>'+
         '   <p>Clicking "Desktop" will bring you back to full screen mode.<\/p>'+
@@ -41,9 +43,9 @@ $(function(){
         '  <\/li>'+
         '  </ol>  ' +
         //'<script src="../../js/vendor/jquery.js">' + '</sc' + 'ript>'+
-        '<script src="../../js/foundation/foundation.js"></script>'+
-        '<script src="../../js/foundation/foundation.joyride.js"></script>'+
-        '<script src="../../js/app/demo-bar-post.js"></script>'+
+        // '<script src="../js/foundation/foundation.js"></script>'+
+        '<script src="../js/foundation/foundation.joyride.js"></script>'+
+        '<script src="../js/app/demo-bar-post.js"></script>'+
         '<\/body><\/html>');
     }());
     window.resbook = {};
@@ -73,10 +75,10 @@ $(function(){
                 f = f || "Desktop";
                 size.innerHTML = f; //w + 'x' + h
             }, setPosition = function (wh, t, cl, myTxt) {
-                var width = (wh == 'auto') ? w.innerWidth : wh[0],
-                    height = (wh == 'auto') ? w.innerHeight : wh[1],
+                var width = (wh === 'auto') ? w.innerWidth : wh[0],
+                    height = (wh === 'auto') ? w.innerHeight : wh[1],
                     style = 'width:' + width + 'px;';
-                if (typeof (width) == 'undefined' || typeof (height) == 'undefined') return false;
+                if (typeof (width) === 'undefined' || typeof (height) === 'undefined') { return false; }
                 style += (wh === 'auto') ? 'margin-top:0;' : '';
                 wrapper.setAttribute('style', style);
                 wrapper.setAttribute('data-device', cl);
@@ -153,7 +155,7 @@ $(function(){
                 wrapper.classList.toggle('keyboard')
             }, false);*/
             w.addEventListener('resize', function () {
-                resize()
+                resize();
             }, false);
             w.addEventListener('keyup', function (e) {
                 var key = e.keyCode ? e.keyCode : e.charCode,
@@ -165,11 +167,11 @@ $(function(){
                         53: 'auto'
                     };
                 if (typeof (keys[key]) == 'undefined') return false;
-                setPosition(sizes[keys[key]], false, keys[key], "auto")
+                setPosition(sizes[keys[key]], false, keys[key], "auto");
             }, false);
             resize();
-            size.style.minWidth = 0
-        })
+            size.style.minWidth = 0;
+        });
     })(window.resbook);
   }
 });

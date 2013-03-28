65489fe268ebdfeec918da6b0a3c2850d0c02af8
Craig Patik <cpatik@gmail.com>
2013-03-28 16:17:44 -0400

    Links to buttons demo, closes #226

--- prototypes/touch/app.js
+++ prototypes/touch/app.js
@@ -1,8 +1,13 @@
+/*global Modernizr: false, exlsr: false */
+
 $(document).ready(function() {
   var $out = $('#out'),
       logType = function (type) {
         $out.prepend('<li>' + type + '</li>');
-      };
+      },
+      $html = $('html'),
+      $mobile = $('#mobile'),
+      $deskop = $('#desktop');
 
   // Touch is supported
   if (Modernizr.touch) {
@@ -23,4 +28,24 @@ $(document).ready(function() {
     .on('swipeRight', function (evt) {
       logType(evt.type);
     });
+
+  // Tap Targets
+
+  // Force desktop display at the start
+  $html
+    .removeClass('touch')
+    .addClass('no-touch');
+
+  $('.switch').on(exlsr.activateEventName, function (evt) {
+    if ($mobile.is(':checked')) {
+      $html
+        .addClass('touch')
+        .removeClass('no-touch');
+    }
+    else {
+      $html
+        .removeClass('touch')
+        .addClass('no-touch');
+    }
+  });
 });

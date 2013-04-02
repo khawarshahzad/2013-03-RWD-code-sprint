0af5a23b746962a1bed752240db6fd14a47702de
Jeff Herb <herb.jeff@gmail.com>
2013-04-02 13:42:36 -0400

    worked with eric to fix joyride bug.

--- js/app/demo-bar-post.js
+++ js/app/demo-bar-post.js
@@ -27,7 +27,7 @@ $("body").on("click", ".joyride-close-tip", function(){ hasClicked = true; });
 $(".joyride-tip-guide").find(".joyride-next-tip").eq(1).remove(); // removes next button on the devices tip, we want them to click a button, NOT click next or else the rest of the stuff won't look right
 $('body').keyup(function(e){
     if(e.which == 27){
-        $(document).foundation("joyride", "hide");
+        $(document).foundation("joyride", "end");
             hasClicked=true;
     }
 });
